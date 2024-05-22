cae367752125728ec26538b8fc9e6d035512baba
Gabriel <lopes.biel2588@gmail.com>
2024-05-22 00:35:29 -0300

    Upgrade

--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -1,24 +1,56 @@
+import React, { useState, useEffect } from 'react';
 import Header from '../../Componentes/HeaderOriginal/Header';
 import BlocosSemanal from '../../Componentes/BlocosSemanal/BlocoSemanal';
 import Progress from '../../Componentes/Progress/App';
 import './Home.css';
 
 function Home() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const url = 'https://script.google.com/macros/s/AKfycbzAYHD26zk69fnnfLq0tuV3259-6fcZ_vlB8ET0PVPr6Z7d3vzFTRyNg16C89CNVGblUQ/exec';
+      const action = 'ReadValorMensal';
+      const token_acess = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
+
+      const apiUrl = `${url}?action=${action}&token_acess=${token_acess}`;
+
+      try {
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+          throw new Error(`Erro na solicitação: ${response.status}`);
+        }
+        const jsonData = await response.json();
+        let valor = 0;
+        for (let i = 0; i < jsonData.length; i++) {
+            valor += jsonData[i];
+        }
+        setData(valor);
+      } catch (error) {
+        console.error(error.message);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+
   return (
     <div className='bodyHome'>
-     
       <div className="blocoTeste">
         <div className="blocosemanais">
-          <BlocosSemanal/>  
+          <BlocosSemanal />  
+        </div>
+        <div className="blocoValorMensal">
+          <p>O valor de vendas está no total de: R$</p><p id='valorMensal'>{data}</p>
         </div>
         <div className="blocoProgresso">
-          <Progress/>
+          <Progress />
         </div>
-        
       </div>
       <Header />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
